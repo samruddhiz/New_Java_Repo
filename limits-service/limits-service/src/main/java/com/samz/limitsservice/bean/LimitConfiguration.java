f0b9126155e6f047a51dee7cdc8342c6358c59cd
samruddhiz <samruddhi.valavalkar@gmail.com>
2021-05-31 16:23:43 +0530

    initial commit

---
+++ limits-service/limits-service/src/main/java/com/samz/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,23 @@
+package com.samz.limitsservice.bean;
+
+public class LimitConfiguration {
+	private int maximum;
+	private int minimum;
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	public LimitConfiguration() {
+		super();
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	
+	
+	
+}
